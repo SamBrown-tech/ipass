abadede1bde822eb04e14ba76321f335961f11b6
santinoHU <santino.b@live.nl>
2018-06-23 17:03:59 +0200

    database connection heroku

--- src/main/java/nl/hu/ipass/gitaarshop/persistence/PostgresBaseDao.java
+++ src/main/java/nl/hu/ipass/gitaarshop/persistence/PostgresBaseDao.java
@@ -1,24 +1,17 @@
 package nl.hu.ipass.gitaarshop.persistence;
 
 import java.sql.Connection;
-
-import javax.sql.DataSource;
-import javax.naming.InitialContext;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class PostgresBaseDao {
-	protected final Connection getConnection() {
-		Connection result = null;
+    private static Connection conn;
+    protected final Connection getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("org.postgresql.Driver");
+        conn = DriverManager.getConnection("postgres://crgdfkpaidnlaa:c7ced32e468ddd9c91ade4f26afada2fd2dacd68e6d8798ca08b861734847d86@ec2-54-247-100-44.eu-west-1.compute.amazonaws.com:5432/d8bvhgrnh2f7fm");
 
-		try {
-			InitialContext ic = new InitialContext();
-			DataSource ds = (DataSource) ic.lookup("java:comp/env/jdbc/PostgresDS");
+        return conn;
 
-			result = ds.getConnection();
-		} catch (Exception ex) {
-			throw new RuntimeException(ex);
-		}
-
-		return result;
-	}
+    }
 
 }
