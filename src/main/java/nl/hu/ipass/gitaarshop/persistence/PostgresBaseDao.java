43a6d6aca045d1039063d462e7654224dfac8058
santinoHU <santino.b@live.nl>
2018-06-23 17:09:24 +0200

    database connection heroku

--- src/main/java/nl/hu/ipass/gitaarshop/persistence/PostgresBaseDao.java
+++ src/main/java/nl/hu/ipass/gitaarshop/persistence/PostgresBaseDao.java
@@ -8,7 +8,7 @@ public class PostgresBaseDao {
     private static Connection conn;
     protected final Connection getConnection() throws SQLException, ClassNotFoundException {
         Class.forName("org.postgresql.Driver");
-        conn = DriverManager.getConnection("postgres://crgdfkpaidnlaa:c7ced32e468ddd9c91ade4f26afada2fd2dacd68e6d8798ca08b861734847d86@ec2-54-247-100-44.eu-west-1.compute.amazonaws.com:5432/d8bvhgrnh2f7fm");
+        conn = DriverManager.getConnection("jdbc:heroku://ec2-54-247-100-44.eu-west-1.compute.amazonaws.com/d8bvhgrnh2f7fm?user=crgdfkpaidnlaa&password=c7ced32e468ddd9c91ade4f26afada2fd2dacd68e6d8798ca08b861734847d86");
 
         return conn;
 
