1032056924678acb6f0f22b08cfbe03700f4ab8d
santinoHU <santino.b@live.nl>
2018-06-27 15:34:51 +0200

    heroku

--- src/main/java/nl/hu/ipass/gitaarshop/persistence/PostgresBaseDao.java
+++ src/main/java/nl/hu/ipass/gitaarshop/persistence/PostgresBaseDao.java
@@ -11,7 +11,6 @@ public class PostgresBaseDao {
 		Class.forName("org.postgresql.Driver");
 		conn = DriverManager.getConnection(
 				"jdbc:postgresql://ec2-54-247-100-44.eu-west-1.compute.amazonaws.com/d8bvhgrnh2f7fm?user=crgdfkpaidnlaa&password=c7ced32e468ddd9c91ade4f26afada2fd2dacd68e6d8798ca08b861734847d86");
-
 		return conn;
 
 	}
